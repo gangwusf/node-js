1ad8f1383806a0b9aadd2979eccfeb845d45f006
Greg Wu <gregwu@yahoo.com>
2022-03-09 11:17:09 -0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+/*
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+*/
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .post('/complete_batch', (req, res) => { res.render('pages/json'); console.log(req.body);})
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
