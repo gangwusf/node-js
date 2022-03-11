e2fd677275aff742559d9be458448d713b802289
Greg Wu <gregwu@yahoo.com>
2022-03-11 09:35:39 -0800

    json

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ express()
   .use(express.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .post('/complete_batch', (req, res) => { res.render('pages/json'); console.log(req.body);})
+  .post('/complete_batch', (req, res) => 
+	{ 
+		res.json(
+{"completions":[
+{"completion":"\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/helloworld\";\n  @import \"example/","log_prob":0.0},
+{"completion":"\n    return \"Hello World!\"\n}", "log_prob":0.0},
+{"completion":"\n  # TEMPLATE STORAGE\n\n  # {{ TEMPLATE STORAGE }}\\\n  # {{ TEMPLATE STORAGE }}\\\n  # {{ TEMPLATE STORAGE }}\\\n\n  # {{ TEMPLATE STORAGE }}\\\n  # {{ TEMPLATE STORAGE }}\\\n  # {{ TEMPLATE STORAGE }}\\\n\n  # {{ TEMPLATE STORAGE }}\\\n  # {{ TEMPLATE STORAGE }}\\\n  # {{ TEMPLATE STORAGE }}\\\n  # {{ TEMPLATE STORAGE }","log_prob":0.0},
+{"completion":"\n    render_string('Hello world')\n}","log_prob":0.0}]}
+		);
+		console.log(req.body);
+	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
